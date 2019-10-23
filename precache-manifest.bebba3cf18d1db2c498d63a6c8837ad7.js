ed1eb9aa2b773e042ea02a5671b66b132d009330
shomi2302 <shoshtar2302@gmail.com>
2019-10-23 18:20:34 +0200

    Updates

---
+++ precache-manifest.bebba3cf18d1db2c498d63a6c8837ad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e91e4db084e945c349c398c608b808e",
+    "url": "/Youtube-Clone/index.html"
+  },
+  {
+    "revision": "3e9e540aa8bc0953d3fc",
+    "url": "/Youtube-Clone/static/css/main.c5dcc36b.chunk.css"
+  },
+  {
+    "revision": "257c54accbe65473e674",
+    "url": "/Youtube-Clone/static/js/2.7f462c10.chunk.js"
+  },
+  {
+    "revision": "3e9e540aa8bc0953d3fc",
+    "url": "/Youtube-Clone/static/js/main.714901d4.chunk.js"
+  },
+  {
+    "revision": "0001e1d98fc9530473e9",
+    "url": "/Youtube-Clone/static/js/runtime-main.c4333d0a.js"
+  }
+]);
